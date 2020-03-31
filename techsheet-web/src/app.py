9123d55925ed0023c7a065c89156257b1eda24c2
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2020-03-31 15:11:25 +0000

    Update landing.

--- techsheet-web/src/app.py
+++ techsheet-web/src/app.py
@@ -2,24 +2,24 @@ from flask import Flask, render_template, redirect
 from src.config import Config
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    @app.route('/')
-    def main():
-        return redirect('bot')
+@app.route('/')
+def main():
+    return redirect('bot')
 
-    @app.route('/bot')
-    def bot():
-        return render_template("index.html")
+@app.route('/bot')
+def bot():
+    return render_template("index.html")
 
-    @app.route('/privacy')
-    def privacy():
-        return render_template("privacy.html")
+@app.route('/privacy')
+def privacy():
+    return render_template("privacy.html")
 
-    @app.route('/terms')
-    def terms():
-        return render_template("terms.html")
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
 
-    return app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)

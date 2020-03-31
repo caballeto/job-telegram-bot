c9ca591636376da9b4d653e081ca162ca0ece23e
Vladyslav Mokrousov <elminsteraumar4@gmail.com>
2020-03-31 16:53:50 +0000

    Update landing 2.

--- bot/config.py
+++ bot/config.py
@@ -4,12 +4,12 @@ import os
 LI_BASE_URL = 'https://www.linkedin.com/comm/jobs/view/'
 
 # email
-EMAIL = os.environ.get('email')
-PASS = os.environ.get('email_pass')
+EMAIL = os.environ.get('EMAIL')
+PASS = os.environ.get('EMAIL_PASS')
 FROM = 'jobalerts-noreply@linkedin.com'
 
 # database
 DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASS = 'root'
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
 DB_NAME = 'LinkedinJobAlerts'
